a079402d6eaeb4b20c55392c9f1c98f45d8ddcc9
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2013-10-22 13:27:30 +0200

    WIP unify DomainServiceImpl & DomainService (having interface DomainService is unneeded and cumbersome)

--- src/main/java/org/esupportail/smsuapi/domain/DomainService.java
+++ src/main/java/org/esupportail/smsuapi/domain/DomainService.java
@@ -1,185 +1,439 @@
 /**
- * ESUP-Portail esup-smsu-api - Copyright (c) 2006 ESUP-Portail consortium
- * http://sourcesup.cru.fr/projects/esup-smsu-api
+ * ESUP-Portail esup-smsu-api-admin - Copyright (c) 2006 ESUP-Portail consortium
+ * http://sourcesup.cru.fr/projects/esup-smsu-api-admin
  */
 package org.esupportail.smsuapi.domain;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.esupportail.commons.exceptions.ConfigException;
 import org.esupportail.commons.exceptions.UserNotFoundException;
-import org.esupportail.commons.services.application.Version;
-
+import org.esupportail.commons.services.logging.Logger;
+import org.esupportail.commons.services.logging.LoggerImpl;
+import org.esupportail.commons.utils.Assert;
+import org.esupportail.smsuapi.business.BlackListManager;
+import org.esupportail.smsuapi.business.ClientManager;
+import org.esupportail.smsuapi.business.ReportingManager;
+import org.esupportail.smsuapi.business.SendSmsManager;
+import org.esupportail.smsuapi.business.SendSmsThirdManager;
+import org.esupportail.smsuapi.dao.DaoService;
 import org.esupportail.smsuapi.dao.beans.Application;
 import org.esupportail.smsuapi.dao.beans.Sms;
 import org.esupportail.smsuapi.domain.beans.User;
+import org.esupportail.smsuapi.domain.beans.sms.SmsStatus;
 import org.esupportail.smsuapi.exceptions.InsufficientQuotaException;
 import org.esupportail.smsuapi.exceptions.UnknownIdentifierApplicationException;
 import org.esupportail.smsuapi.exceptions.UnknownIdentifierMessageException;
 import org.esupportail.smsuapi.exceptions.UnknownMonthIndexException;
 import org.esupportail.ws.remote.beans.ReportingInfos;
+import org.springframework.beans.factory.InitializingBean;
+
 
 
 /**
- * The domain service interface.
+ * The basic implementation of DomainService.
+ * 
+ * See /properties/domain/domain-example.xml
  */
-public interface DomainService extends Serializable {
+public class DomainService implements InitializingBean {
+
+	/**
+	 * The serialization id.
+	 */
+	private static final long serialVersionUID = -8200845058340254019L;
+
+	/**
+	 * {@link DaoService}.
+	 */
+	private DaoService daoService;
+
+	/**
+	 * {@link SendSmsManager}.
+	 */
+	private SendSmsManager sendSmsManager;
+
+	/**
+	 * {@link SendSmsThirdManager}.
+	 */
+	private SendSmsThirdManager sendSmsThirdManager;
+
+	/**
+	 * {@link BlackListManager}.
+	 */
+	private BlackListManager blackListManager;
+
+	/**
+	 * {@link ReportingManager}.
+	 */
+	private ReportingManager reportingManager;
+
+	/**
+	 *  {@link ClientManager}.
+	 */
+	private ClientManager clientManager;
+
+	/**
+	 * A logger.
+	 */
+	private final Logger logger = new LoggerImpl(getClass());
+
+	/**
+	 * Bean constructor.
+	 */
+	public DomainService() {
+		super();
+	}
+
+	/**
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(this.daoService, 
+				"property daoService of class " + this.getClass().getName() + " can not be null");
+	}
 
 	//////////////////////////////////////////////////////////////
 	// User
 	//////////////////////////////////////////////////////////////
 
 	/**
-	 * @param id
-	 * @return the User instance that corresponds to an id.
-	 * @throws UserNotFoundException
+	 * Set the information of a user.
+	 * @param user 
+	 * @return true if the user was updated.
 	 */
-	User getUser(String id) throws UserNotFoundException;
+	private boolean setUserInfo(
+			final User user) {
+		String displayName = user.getId();
+		if (displayName.equals(user.getDisplayName())) {
+			return false;
+		}
+		user.setDisplayName(displayName);
+		return true;
+	}
+
 
 	/**
-	 * @return the list of all the users.
+	 * @see org.esupportail.smsuapi.domain.DomainService#updateUserInfo(org.esupportail.smsuapi.domain.beans.User)
 	 */
-	List<User> getUsers();
+	public void updateUserInfo(final User user) {
+		if (setUserInfo(user)) {
+			updateUser(user);
+		}
+	}
 
 	/**
-	 * Update a user.
-	 * @param user
+	 * If the user is not found in the database, try to create it from a LDAP search.
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#getUser(java.lang.String)
 	 */
-	void updateUser(User user);
+	public User getUser(final String id) throws UserNotFoundException {
+		User user = daoService.getUser(id);
+		if (user == null) {
+			user = new User();
+			user.setId(id);
+			setUserInfo(user);
+			daoService.addUser(user);
+			logger.info("user '" + user.getId() + "' has been added to the database");
+		}
+		return user;
+	}
 
 	/**
-	 * Update a user's information (retrieved from the LDAP directory for instance).
-	 * @param user
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#getUsers()
 	 */
-	void updateUserInfo(User user);
-	
-	/**
-	 * Add an administrator.
-	 * @param user
-	 */
-	void addAdmin(User user);
+	public List<User> getUsers() {
+		return this.daoService.getUsers();
+	}
 
 	/**
-	 * Delete an administrator.
-	 * @param user
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#updateUser(
+	 * org.esupportail.smsuapiadmin.domain.beans.User)
 	 */
-	void deleteAdmin(User user);
-	
+	public void updateUser(final User user) {
+		this.daoService.updateUser(user);
+	}
+
+	/**
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#addAdmin(
+	 * org.esupportail.smsuapiadmin.domain.beans.User)
+	 */
+	public void addAdmin(
+			final User user) {
+		user.setAdmin(true);
+		updateUser(user);
+	}
+
+	/**
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#deleteAdmin(
+	 * org.esupportail.smsuapiadmin.domain.beans.User)
+	 */
+	public void deleteAdmin(
+			final User user) {
+		user.setAdmin(false);
+		updateUser(user);
+	}
+
+
 	//////////////////////////////////////////////////////////////
 	// Authorizations
 	//////////////////////////////////////////////////////////////
 
 	/**
-	 * @param currentUser
-	 * @return 'true' if the user can view administrators.
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#userCanViewAdmins(
+	 * org.esupportail.smsuapiadmin.domain.beans.User)
 	 */
-	boolean userCanViewAdmins(User currentUser);
-	
-	/**
-	 * @param user 
-	 * @return 'true' if the user can grant the privileges of administrator.
-	 */
-	boolean userCanAddAdmin(User user);
+	public boolean userCanViewAdmins(final User user) {
+		if (user == null) {
+			return false;
+		}
+		return user.getAdmin();
+	}
 
 	/**
-	 * @param user 
-	 * @param admin
-	 * @return 'true' if the user can revoke the privileges of an administrator.
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#userCanAddAdmin(
+	 * org.esupportail.smsuapiadmin.domain.beans.User)
 	 */
-	boolean userCanDeleteAdmin(User user, User admin);
+	public boolean userCanAddAdmin(final User user) {
+		if (user == null) {
+			return false;
+		}
+		return user.getAdmin();
+	}
+
+	/**
+	 * @see org.esupportail.smsuapiadmin.domain.DomainService#userCanDeleteAdmin(
+	 * org.esupportail.smsuapiadmin.domain.beans.User, org.esupportail.smsuapiadmin.domain.beans.User)
+	 */
+	public boolean userCanDeleteAdmin(final User user, final User admin) {
+		if (user == null) {
+			return false;
+		}
+		if (!user.getAdmin()) {
+			return false;
+		}
+		return !user.equals(admin);
+	}
+
+	//////////////////////////////////////////////////////////////
+	// Misc
+	//////////////////////////////////////////////////////////////
+
+	/**
+	 * @param daoService the daoService to set
+	 */
+	public void setDaoService(final DaoService daoService) {
+		this.daoService = daoService;
+	}
 
 	//////////////////////////////////////////////////////////////
 	// WS SendTrack methods
 	//////////////////////////////////////////////////////////////
 	/**
-	 * @param msgId 
 	 * @return the number of SMS recipients.
+	 * @throws UnknownIdentifierMessageException 
+	 * @throws UnknownIdentifierApplicationException 
 	 */
-	int getNbDest(Integer msgId) throws UnknownIdentifierMessageException, UnknownIdentifierApplicationException;
-	
+	public int getNbDest(final Integer msgId) 
+	throws UnknownIdentifierMessageException, UnknownIdentifierApplicationException {
+		Application app = clientManager.getApplication();
+		{
+			if (daoService.getNbDest(msgId, app) == 0) {
+				throw new UnknownIdentifierMessageException("Unknown message");
+			} else {
+				return daoService.getNbDest(msgId, app);
+			}
+		}	
+	}
+
 	/**
-	 * @param msgId 
 	 * @return the non-authorized phone numbers (in black list).
+	 * @throws UnknownIdentifierApplicationException 
 	 */
-	int getNbDestBlackList(Integer msgId) throws UnknownIdentifierApplicationException;
-	
+	@SuppressWarnings("unchecked")
+	public int getNbDestBlackList(final Integer msgId) throws UnknownIdentifierApplicationException {
+		List<String> list = new ArrayList();
+		list.add(SmsStatus.ERROR_PRE_BL.name());
+		list.add(SmsStatus.ERROR_POST_BL.name());
+
+		Application app = clientManager.getApplication();
+		return daoService.getNbSmsWithState(msgId, app, list);
+
+
+	}
+
 	/**
-	 * @param msgId 
 	 * @return the number of sent SMS.
+	 * @throws UnknownIdentifierApplicationException 
 	 */
-	int getNbSentSMS(Integer msgId) throws UnknownIdentifierApplicationException;
+	public int getNbSentSMS(final Integer msgId) throws UnknownIdentifierApplicationException {
+		Application app = clientManager.getApplication();
+		return daoService.getNbSentSMS(msgId, app);
+
+
+	}
 
 	/**
 	 * @return the number of SMS in progress.
-	 */
-	int getNbProgressSMS(Integer msgId) throws UnknownIdentifierApplicationException;
-	
-	/**
-	 * @return the number of SMS in error.
-	 */
-	int getNbErrorSMS(Integer msgId) throws UnknownIdentifierApplicationException;
-	
-	/**
-	 * @return the number of SMS in error.
-	 */
-	Set<String> getListNumErreur(Integer msgId);
-
-	/**
-	 * @return the sms object corresponding to msgId & phoneNumber
 	 * @throws UnknownIdentifierApplicationException 
 	 */
-	Sms getSms(Integer msgId, String phoneNumber) throws UnknownIdentifierApplicationException;
+	public int getNbProgressSMS(final Integer msgId) throws UnknownIdentifierApplicationException {
+		Application app = clientManager.getApplication();
+		return daoService.getNbProgressSMS(msgId, app);
+	}
+
+	/**
+	 * @return the number of SMS in error.
+	 * @throws UnknownIdentifierApplicationException 
+	 * @throws Exception 
+	 */
+	@SuppressWarnings("unchecked")
+	public int getNbErrorSMS(final Integer msgId) throws UnknownIdentifierApplicationException {
+		List<String> list = new ArrayList();
+		list.add(SmsStatus.ERROR.name());
+		list.add(SmsStatus.ERROR_PRE_BL.name());
+		list.add(SmsStatus.ERROR_POST_BL.name());
+
+		Application app = clientManager.getApplication();
+		return daoService.getNbErrorSMS(msgId, app, list);
+
+	}
+
+	/**
+	 * @return the number of SMS in error.
+	 */
+	@SuppressWarnings("unchecked")
+	public Set<String> getListNumErreur(final Integer msgId) {
+		// list criteria for HQL query 
+		List<String> list = new ArrayList();
+		list.add(SmsStatus.ERROR.name());
+		list.add(SmsStatus.ERROR_POST_BL.name());
+		list.add(SmsStatus.ERROR_PRE_BL.name());
+
+		List<Sms> smslist = new ArrayList<Sms>();
+
+		Application app = clientManager.getApplicationOrNull();
+
+		if (app != null) {
+			// Retrieve list of phones numbers 	
+			smslist = daoService.getListNumErreur(msgId, app, list);
+		}
+		
+		Set<String> nums = new HashSet<String>();
+		for (Sms sms : smslist) nums.add(sms.getPhone());
+		return nums;
+	}
+
+	public Sms getSms(Integer msgId, String phoneNumber) throws UnknownIdentifierApplicationException {
+		Application app = clientManager.getApplication();
+		
+		List<Sms> l = daoService.getSms(app, msgId, phoneNumber);
+		if (l.size() == 0) {
+			return null;
+		} else if (l.size() > 1) {
+			logger.error("internal error: more than one sms corresponds to " + msgId + ":" + phoneNumber);
+			return null;
+		} else {
+			return l.get(0);
+		}
+	}
 
 	//////////////////////////////////////////////////////////////
 	// WS SendSms methods
 	//////////////////////////////////////////////////////////////
 	/**
-	 * @param nbDest 
-	 * @param labelAccount 
+	 * @see org.esupportail.smsuapi.services.remote.SendSms#getQuota()
 	 */
-	void mayCreateAccountCheckQuotaOk(Integer nbDest, String labelAccount) 
+	public void mayCreateAccountCheckQuotaOk(final Integer nbDest, final String labelAccount) 
 	throws UnknownIdentifierApplicationException, 
-	InsufficientQuotaException;
-	
+	InsufficientQuotaException {
+		sendSmsManager.mayCreateAccountAndCheckQuotaOk(nbDest, labelAccount);
+	}
+
 	/**
-	 * send SMS.
 	 * @param msgId 
 	 * @param perId 
 	 * @param smsPhone 
 	 * @param labelAccount 
 	 * @param msgContent 
 	 * @throws UnknownIdentifierApplicationException 
+	 * @see org.esupportail.smsuapi.services.remote.SendSms#snrdSMS()
 	 */
-	void sendSMS(Integer msgId, Integer perId, 
-			String smsPhone, String labelAccount, 
-			String msgContent);
+	public void sendSMS(final Integer msgId, final Integer perId,
+			final String smsPhone, 
+			final String labelAccount, final String msgContent) {
+		sendSmsManager.sendSMS(msgId, perId, smsPhone, labelAccount, msgContent);	
+	}
 
 	//////////////////////////////////////////////////////////////
-	// WS SendSmsByThird methods
+	// WS SendSmsThird methods
 	//////////////////////////////////////////////////////////////
-	void sendSMSByThird(List<String> smsPhoneList,
-			 String msgContent, int msgId) throws UnknownIdentifierApplicationException, 
-				InsufficientQuotaException; 
-	
-	
-	/////////////////////////////////////////////////////////////
-	// WS BlackList methods
+	/**
+	 * @throws InsufficientQuotaException 
+	 * @see org.esupportail.smsuapi.services.remote.SendSms#snrdSMS()
+	 */
+	public void sendSMSByThird(final List<String> smsPhoneList, final String msgContent, final int msgId)
+	throws UnknownIdentifierApplicationException, 
+	InsufficientQuotaException {
+
+		sendSmsThirdManager.sendSMSByThird(smsPhoneList, msgContent, msgId);
+	}
+
+
+	//////////////////////////////////////////////////////////////
+	// WS Blacklist methods
 	//////////////////////////////////////////////////////////////
 	/**
 	 * Test if a phone number is already in the black list.
 	 * @param phoneNumber
 	 * @return return true if the phone number is in the bl, false otherwise
 	 */
-	boolean isPhoneNumberInBlackList(String phoneNumber);
-	
+	public boolean isPhoneNumberInBlackList(final String phoneNumber) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Request in domaineService : " + phoneNumber);
+		}
+		Boolean retVal = blackListManager.isPhoneNumberInBlackList(phoneNumber); 
+		if (logger.isDebugEnabled()) {
+			logger.debug("Response return in domaineService for : " 
+					+ phoneNumber + " is : " + retVal);
+		}
+
+		return retVal;
+	}
+
 	/**
 	 * Test if a phone number is already in the black list.
 	 * @param phoneNumber
 	 * @return return true if the phone number is in the bl, false otherwise
 	 */
-	Set<String>  getListPhoneNumbersInBlackList();
+	public Set<String> getListPhoneNumbersInBlackList() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Request in domainService for ListPhoneNumbersInBlackList");
+		}
+		Set<String> listPhoneNumbersInBlackList = blackListManager.getListPhoneNumbersInBlackList();
+
+		if (logger.isDebugEnabled()) {
+			final StringBuilder sb = new StringBuilder(500);
+			sb.append("Response in domainService for ListPhoneNumbersInBlackList :");
+			for (String nb : listPhoneNumbersInBlackList) {
+				sb.append(" - phone number in blacklist = ").append(nb);	
+			}
+			logger.debug(sb.toString());
+		}
+		return listPhoneNumbersInBlackList;
+	}
+
+
+
+
+	/**
+	 * @see org.esupportail.smsuapi.domain.DomainService#getApplicationByName(java.lang.String)
+	 */
+	public Application getApplicationByName(final String name) {
+		return daoService.getApplicationByName(name);
+	}
+
 
 	/////////////////////////////////////////////////////////////
 	// WS Reporting method
@@ -189,18 +443,47 @@ public interface DomainService extends Serializable {
 	 * @param month
 	 * @param year
 	 */
-	ReportingInfos getStats(int month, int year) 
-					throws UnknownMonthIndexException, UnknownIdentifierApplicationException;
-	
-	/**
-	 * @param name
-	 * @return the application dao
-	 */
-	Application getApplicationByName(String name);
+	public ReportingInfos getStats(final int month, final int year) 
+	throws UnknownMonthIndexException, UnknownIdentifierApplicationException {
+		ReportingInfos reportingInfos = reportingManager.getStats(month, year);
+		return reportingInfos;
+	}
 
 	/**
-	 * @return a function to test the connexion.
+	 * @see org.esupportail.smsuapi.domain.DomainService#testConnexion()
 	 */
-	String testConnexion();
-	
+	public String testConnexion() {
+
+		String sReturn = "Application reconnue : ";
+		String application = clientManager.getClientName();
+
+		if (application.equals(""))
+		    return "ERROR : " + clientManager.getNoCertificateNorBasicAuthErrorMessage();
+		else 
+		    return sReturn + application;
+	}
+
+	///////////////////////////////////////
+	//  Mutators
+	//////////////////////////////////////
+	public void setSendSmsManager(final SendSmsManager sendSmsManager) {
+		this.sendSmsManager = sendSmsManager;
+	}
+
+	public void setSendSmsThirdManager(final SendSmsThirdManager sendSmsThirdManager) {
+		this.sendSmsThirdManager = sendSmsThirdManager;
+	}
+
+	public void setBlackListManager(final BlackListManager blackListManager) {
+		this.blackListManager = blackListManager;
+	}
+
+	public void setReportingManager(final ReportingManager reportingManager) {
+		this.reportingManager = reportingManager;
+	}
+
+	public void setClientManager(final ClientManager clientManager) {
+		this.clientManager = clientManager;
+	}
+
 }
