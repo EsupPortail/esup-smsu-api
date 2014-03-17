9e265c4be267baa05a38ae24d9d076990615d121
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2014-03-17 10:54:57 +0100

    test: add OptionalTestSpringBeans to test spring beans

---
+++ src/test/java/org/esupportail/smsuapi/OptionalTestSpringBeans.java
@@ -0,0 +1,17 @@
+package org.esupportail.smsuapi;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/* not run by default. You can use the following to run it: 
+   mvn -Dtest=OptionalTestSpringBeans test
+*/
+public class OptionalTestSpringBeans {
+
+	@Test
+	public void testSpringBeans() {
+		@SuppressWarnings({ "unused", "resource" })
+		ClassPathXmlApplicationContext unused = new ClassPathXmlApplicationContext("properties/applicationContext.xml");
+	}
+
+}
