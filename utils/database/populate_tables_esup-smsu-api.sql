808b12415dab7d680b5e55b9e3d177e7bbd5e9d0
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2014-04-10 18:06:36 +0200

    populate_tables_esup-smsu-api.sql: replace mysql syntax with portable SQL (esp. for HSQL)

--- utils/database/populate_tables_esup-smsu-api.sql
+++ utils/database/populate_tables_esup-smsu-api.sql
@@ -1,22 +1,17 @@
-/*==============================================================*/
-/* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de création :  22/06/2009 12:01:44                      */
-/*==============================================================*/
-
-/*==============================================================*/
-/* Insert Init Data		                                        */
-/*==============================================================*/
+-- ==============================================================
+--  Insert Init Data		                                        
+-- ==============================================================
 
 -- Ajout des fonctions
-INSERT INTO fonction VALUES (1, "FCTN_API_EDITION_RAPPORT");
-INSERT INTO fonction VALUES (2, "FCTN_GESTION_CPT_IMPUT");
-INSERT INTO fonction VALUES (3, "FCTN_API_CONFIG_APPLIS");
-INSERT INTO fonction VALUES (4, "FCTN_MANAGE_USERS");
+INSERT INTO fonction VALUES (1, 'FCTN_API_EDITION_RAPPORT');
+INSERT INTO fonction VALUES (2, 'FCTN_GESTION_CPT_IMPUT');
+INSERT INTO fonction VALUES (3, 'FCTN_API_CONFIG_APPLIS');
+INSERT INTO fonction VALUES (4, 'FCTN_MANAGE_USERS');
 
 -- Ajout des rôles
-INSERT INTO role VALUES (1, "ROLE_MANAGE");
-INSERT INTO role VALUES (2, "ROLE_REPORT");
-INSERT INTO role VALUES (3, "ROLE_SUPER_ADMIN");
+INSERT INTO role VALUES (1, 'ROLE_MANAGE');
+INSERT INTO role VALUES (2, 'ROLE_REPORT');
+INSERT INTO role VALUES (3, 'ROLE_SUPER_ADMIN');
 
 -- Association des fonctions aux rôles
 INSERT INTO role_composition VALUES (1, 2);
@@ -28,6 +23,6 @@ INSERT INTO role_composition VALUES (3, 3);
 INSERT INTO role_composition VALUES (3, 4);
 
 -- Ajout du premier super administrateur
-INSERT INTO user_bo_smsu VALUES (1, 3, "admin");
+INSERT INTO user_bo_smsu VALUES (1, 3, 'admin');
 
 commit;
