7c2891a0184ca88b2640d6d2fc10a698eba555ed
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2014-07-13 15:57:13 +0200

    migrate*.sql: quartz table names are uppercased

--- src/main/resources/database/migrate_2.0.sql
+++ src/main/resources/database/migrate_2.0.sql
@@ -1,8 +1,8 @@
 alter table application modify APP_CERTIFCATE blob;
 
-delete from qrtz_cron_triggers where TRIGGER_NAME = 'purgeStatisticTrigger';
-delete from qrtz_triggers where JOB_NAME = 'purgeStatisticJob';
-delete from qrtz_job_details where JOB_CLASS_NAME = 'org.esupportail.smsuapi.services.scheduler.job.PurgeStatisticJob';
+delete from QRTZ_CRON_TRIGGERS where TRIGGER_NAME = 'purgeStatisticTrigger';
+delete from QRTZ_TRIGGERS where JOB_NAME = 'purgeStatisticJob';
+delete from QRTZ_JOB_DETAILS where JOB_CLASS_NAME = 'org.esupportail.smsuapi.services.scheduler.job.PurgeStatisticJob';
 
 -- sms with no initialId now gets automatically an initialId
 -- (below is mysql only, sorry...)
