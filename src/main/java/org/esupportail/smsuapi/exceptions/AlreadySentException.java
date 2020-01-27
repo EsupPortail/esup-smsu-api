155ffadb6881cce01e4e67197457b712eb3859b8
hagneva1 <hagneva1@dgs-13-15ab-1752.univ-rouen.fr>
2020-01-27 11:59:43 +0100

    AlreadySentException extends InvalidParameterException

--- src/main/java/org/esupportail/smsuapi/exceptions/AlreadySentException.java
+++ src/main/java/org/esupportail/smsuapi/exceptions/AlreadySentException.java
@@ -1,7 +1,7 @@
 package org.esupportail.smsuapi.exceptions;
 
 @SuppressWarnings("serial")
-public class AlreadySentException extends RuntimeException {
+public class AlreadySentException extends InvalidParameterException {
 
     public AlreadySentException(String err) {
         super(err);
