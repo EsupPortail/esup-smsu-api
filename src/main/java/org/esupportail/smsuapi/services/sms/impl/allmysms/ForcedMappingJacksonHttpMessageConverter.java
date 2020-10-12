f6e1a49778d0bc2cc8c9cb0acb298c235c52c323
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-10-12 22:26:31 +0200

    upgrade to spring 4 (require jackson2 & junit >= 4.12)

--- src/main/java/org/esupportail/smsuapi/services/sms/impl/allmysms/ForcedMappingJacksonHttpMessageConverter.java
+++ src/main/java/org/esupportail/smsuapi/services/sms/impl/allmysms/ForcedMappingJacksonHttpMessageConverter.java
@@ -2,11 +2,11 @@ package org.esupportail.smsuapi.services.sms.impl.allmysms;
 
 import java.util.Arrays;
 
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.MediaType;
 
 /* needed until allmysms correctly set it's response Content-Type to application/json instead of text/html */
-public class ForcedMappingJacksonHttpMessageConverter extends MappingJacksonHttpMessageConverter {
+public class ForcedMappingJacksonHttpMessageConverter extends MappingJackson2HttpMessageConverter {
 
     public ForcedMappingJacksonHttpMessageConverter() {
         setSupportedMediaTypes(Arrays.asList(MediaType.TEXT_HTML));
